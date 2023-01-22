7618abde68e6b5c2f5528e9a7491636b28b2eb73
Denis-Egorov-21IS <123110997+Denis-Egorov-21IS@users.noreply.github.com>
2023-01-22 22:13:14 +0300

    first commit

---
+++ HomeWork№2.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim num1, num2, result As Integer
+
+        num1 = Console.ReadLine()
+        num2 = Console.ReadLine()
+
+        If num1 * num2 <= Int32.MaxValue Then
+            result = num1 * num2
+        Else
+            Console.WriteLine("Переполнение памяти! Расчет выполнен для Num = {0}")
+        End If
+
+        Console.WriteLine("Num * Num = {0}", result)
+    End Sub
+End Module
