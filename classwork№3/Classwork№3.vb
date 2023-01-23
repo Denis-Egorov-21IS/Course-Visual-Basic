181428f87aeecf1082efd8831b91f0637f67ce8d
Denis-Egorov-21IS <123110997+Denis-Egorov-21IS@users.noreply.github.com>
2023-01-23 10:00:45 +0300

    23.01.2023

---
+++ classwork№3/Classwork№3.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim C As Integer
+        Dim Pr As Integer = 1
+        Do
+            Console.Write("Введите множитель:")
+            C = Console.ReadLine()
+            If C < Int32.MaxValue / Pr Then
+                Pr = C * Pr
+            Else
+                Console.WriteLine("Переполнение памяти")
+                Exit Do
+            End If
+
+        Loop Until C = 1
+        Console.WriteLine("Произведение = {0}", Pr)
+    End Sub
+End Module
