ac4741bee1ba5b36d16b45c37509bd8011291778
Denis-Egorov-21IS <123110997+Denis-Egorov-21IS@users.noreply.github.com>
2023-01-29 22:16:20 +0300

    Add files via upload

---
+++ ClassWork№4.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim l As Integer = 10
+        Console.SetCursorPosition(l, 10)
+        For j = 0 To 5
+            For i As Integer = 0 To 10
+                Console.Write("H")
+            Next
+            Console.SetCursorPosition(l, Console.GetCursorPosition().Top + 1)
+        Next
+    End Sub
+End Module
