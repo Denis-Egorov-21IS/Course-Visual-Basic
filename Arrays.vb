7b567ddd33acffdf528ec765897f66b2c6ef5e65
Denis-Egorov-21IS <123110997+Denis-Egorov-21IS@users.noreply.github.com>
2023-03-30 09:38:14 +0300

    mix man

--- Arrays.vb
+++ Arrays.vb
@@ -5,36 +5,45 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Enter 5 numbers")
         Dim input(4) As Integer
-
         For i = 0 To 4
             input(i) = Console.ReadLine
         Next
-
-        Dim num As Integer
-
-        Console.WriteLine("Какое число искать")
-
-        num = Console.ReadLine
-
-        Console.WriteLine(find(input, num))
-
-        Dim min As Integer = input(0)
-        Dim max As Integer = input(0)
-
-        For i As Integer = 1 To input.Length - 1
-            If (input(i) < min) Then
-                min = input(i)
-            End If
-
-            If (input(i) > max) Then
-                max = input(i)
-            End If
-        Next
-
-        Console.WriteLine("Минимальное значение: {0}", min)
-        Console.WriteLine("Максимальное значение: {0}", max)
+        'Dim n As Integer = Max(input)
+        'Dim j As Integer = Min(input)
+        'Console.WriteLine("input ({0}) = {1}", n, input(n))
+        'Console.WriteLine("input ({0}) = {1}", j, input(j))
+        Add(input, 999, 2)
+        print(input)
 
     End Sub
+
+    Sub Add(ByRef list As Integer(), value As Integer, index As Integer)
+        If index < 0 Or index > list.Length Then Exit Sub
+        ReDim Preserve list(list.Length)
+
+        For i = list.Length - 1 To index + 1 Step -1
+            list(i) = list(i - 1)
+        Next
+
+        list(index) = value
+    End Sub
+
+    Function Max(list As Integer()) As Integer
+        Max = 0
+        For i = 0 To 4
+            If list(i) > list(Max) Then
+                Max = i
+            End If
+        Next
+    End Function
+    Function Min(list As Integer()) As Integer
+        Min = 0
+        For i = 0 To 4
+            If list(i) < list(Min) Then
+                Min = i
+            End If
+        Next
+    End Function
 
     Sub print(list As Integer())
         For i = 0 To list.Length - 1
